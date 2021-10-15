7f7f8f9a5c30eeeed9ea394c563e33422445d335
NerdPixel <joh.hackler@gmail.com>
2021-10-15 16:14:07 +0200

    add mikrofon

--- test_station.py
+++ test_station.py
@@ -3,12 +3,8 @@ import time
 
 import RPi.GPIO as GPIO
 
-if __name__ == '__main__':
-    vibrations = 26
 
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(vibrations, GPIO.OUT)
-
+def test_vibration(vibrations):
     for i in range(10):
         logging.info("Vibrate!!!")
         GPIO.output(vibrations, GPIO.HIGH)
@@ -17,3 +13,15 @@ if __name__ == '__main__':
         time.sleep(0.2)
 
     GPIO.cleanup()
+
+def test_sound(sound):
+    for i in range(10)
+        input = GPIO.input(sound)
+        logging.info(input)
+
+if __name__ == '__main__':
+    vibrations = 37
+    sound = 15
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(vibrations, GPIO.OUT)
+    GPIO.setup(sound, GPIO.IN)
