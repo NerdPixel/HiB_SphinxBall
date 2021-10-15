7722330fbf57ef53a10223f078c414afc7aa9f46
NerdPixel <joh.hackler@gmail.com>
2021-10-15 20:10:18 +0200

    add test_gyro

--- test_station.py
+++ test_station.py
@@ -1,6 +1,8 @@
 import logging
 import time
 
+from mpu6050 import mpu6050
+import Adafruit_ADS1x15
 import RPi.GPIO as GPIO
 
 
@@ -14,11 +16,25 @@ def test_vibration(vibrations):
 
 
 def test_sound(sound):
-    for i in range(10):
-        input = GPIO.input(sound)
-        logging.info(input)
-        time.sleep(0.1)
+    ADS1115 = Adafruit_ADS1x15.ADS1115()
+    GAIN = 1
+    print('MAX9814 Microphone Module test script')
+    print('[Press CTRL + C to end the script!]')
+    try:
+        while True:
+            analog = ADS1115.read_adc(0, gain=GAIN)
+    print('Analog: {}'.format(analog))
+    time.sleep(0.002)
+    except KeyboardInterrupt:
+        print('\nScript end!')
+    finally:
+        GPIO.cleanup()
 
+def test_gyro():
+    sensor = mpu6050(0x68)
+    for i in range(10):
+        accelerometer_data = sensor.get_accel_data()
+        logging.info(accelerometer_data)
 
 if __name__ == '__main__':
     logging.basicConfig(filename='test_station.log', level=logging.INFO)
@@ -29,8 +45,8 @@ if __name__ == '__main__':
     GPIO.setup(vibrations, GPIO.OUT)
     GPIO.setup(sound, GPIO.IN)
 
-    test_vibration(vibrations)
-    test_sound(sound)
-
+    #test_vibration(vibrations)
+    #test_sound(sound)
+    test_gyro()
     GPIO.cleanup()
 
