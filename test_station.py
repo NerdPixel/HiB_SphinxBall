f129d66ede4a9180b0b3739457f6ca20b67b57ba
NerdPixel <joh.hackler@gmail.com>
2021-10-15 15:53:57 +0200

    add test_station

---
+++ test_station.py
@@ -0,0 +1,17 @@
+import time
+
+import RPi.GPIO as GPIO
+
+if __name__ == '__main__':
+    vibrations = 26
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(vibrations, GPIO.OUT)
+
+    for i in range(10):
+        GPIO.output(vibrations, GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(vibrations, GPIO.LOW)
+        time.sleep(0.2)
+
+    GPIO.cleanup()
